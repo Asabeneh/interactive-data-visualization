d54925dc659a1e73ba2c77d35f823f86f501d060
asabeneh <asabeneh@gmail.com>
2021-05-03 21:33:00 +0300

    minor fixes

--- js/d3-map.js
+++ js/d3-map.js
@@ -13,7 +13,7 @@ const createD3Map = () => {
       `;
 		});
 
-	const margin = { top: -200, right: -200, bottom: 0, left: 0 },
+	const margin = { top: -250, right: -200, bottom: 0, left: 0 },
 		width = 960 - margin.left - margin.right,
 		height = 500 - margin.top - margin.bottom;
 
@@ -125,72 +125,3 @@ const createD3Map = () => {
 }
 createD3Map()
 
-// const url = 'https://restcountries.eu/rest/v2/all';
-// const countriesData =[]
-// async function fetchData () {
-//   const response = await fetch(url)
-//   const data = await response.json()
-//   for(const item of data){
-//     console.log(item)
-//     countriesData.push(item)
-//   }
-//   console.log(countriesData)
-// console.log(JSON.stringify(countriesData, undefined, 4));
-
-// }
-
-// console.log(fetchData())
-// console.log(countriesData)
-
-
-// console.log(JSON.stringify(countriesData, undefined, 4))
-
-
-// console.log(
-// 	JSON.stringify(
-// 		data.map((item, index) => {
-// 			let rank = index + 1;
-// 			item.Happiness_Rank = rank;
-// 			return item;
-// 		}),
-// 		undefined,
-// 		4,
-// 	),
-// );
-// Happiness_Rank,
-// 	Country,
-// 	Happiness_Score,
-// 	GDP_per_Capita,
-// 	Social_support,
-// 	Healthy_Life_Expectancy,
-// 	Freedom,
-// 	Generosity,
-// 	Perceptions_Of_Corruption;
-
-
-const url = './world_countries.json';
-const countriesData =[]
-const happiness = []
-const gdp = []
-async function fetchData () {
-  const response = await fetch(url)
-  const data = await response.json()
-  const whrResponse = await fetch('./data/whr_2015.json');
-  const whrResponseData = await whrResponse.json()
-console.log(whrResponseData)
-  console.log(
-		JSON.stringify(
-			data.features.map((item) => {
-				let happiness = whrResponseData.filter(
-					(c) => {
-            console.log(item.properties.name)
-            return c.Country == item.properties.name;
-          }
-				)[0];
-				return Object.assign({}, item, { happiness: {year:215, happiness:happiness} });
-			})
-		,undefined, 4),
-	);
-}
-
-// fetchData()
